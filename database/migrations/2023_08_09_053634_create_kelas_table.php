056bff9e3591ec5b26febde9ac97e83b9ae9a2a6
nazwazela <nazwazela@gmail.com>
2023-08-09 16:17:57 +0700

    laravel

---
+++ database/migrations/2023_08_09_053634_create_kelas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_kelas');
+            $table->string('kelas');
+            $table->string('jurusan');
+            $table->string('nama_siswa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kelas');
+    }
+};
