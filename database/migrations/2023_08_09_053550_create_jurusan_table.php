fb0fb48e18ec23215f24b898da92dfd3dfbf75c1
wawazella <nazwazela@gmail.com>
2023-08-09 17:02:53 +0700

    update

---
+++ database/migrations/2023_08_09_053550_create_jurusan_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jurusan', function (Blueprint $table) {
+            $table->id();
+            $table->string('jurusan');
+            $table->string('id_kelas');
+            $table->string('id_mapel');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jurusan');
+    }
+};
